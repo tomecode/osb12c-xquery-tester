44209a709eb81539da5a4c951c3f0aa758451c5f
Tome <tfrastia@gmail.com>
2017-03-17 08:06:29 +0100

    generate test report

--- osb12c-xquery-unit-tester/src/main/java/com/tomecode/oracle/osb12c/x2004/TestCaseX2004.java
+++ osb12c-xquery-unit-tester/src/main/java/com/tomecode/oracle/osb12c/x2004/TestCaseX2004.java
@@ -11,11 +11,36 @@ import junit.framework.TestCase;
  */
 public class TestCaseX2004 extends TestCase {
 
+	/**
+	 * load {@link Xquery} from file
+	 * 
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 */
 	public final Xquery xq(File file) throws Exception {
 		return new XqueryExecutor().loadXquery(file);
 	}
 
+	/**
+	 * load {@link Xquery} from file
+	 * 
+	 * @param file
+	 *            path to xquery file
+	 * @return
+	 * @throws Exception
+	 */
 	public final Xquery xq(String file) throws Exception {
 		return new XqueryExecutor().loadXquery(new File(file).getAbsoluteFile());
+	}
+
+	/**
+	 * generate test report
+	 * 
+	 * @param xquery
+	 * @return
+	 */
+	public final XqueryTestReport report(Xquery xquery) {
+		return new XqueryTestReport(xquery);
 	}
 }
--- osb12c-xquery-unit-tester/src/test/java/com/tomecode/oracle/osb12c/x2004/TestReport.java
+++ osb12c-xquery-unit-tester/src/test/java/com/tomecode/oracle/osb12c/x2004/TestReport.java
@@ -2,6 +2,8 @@ package com.tomecode.oracle.osb12c.x2004;
 
 import java.io.File;
 
+import org.junit.Assert;
+
 /**
  * 
  * @author Tome
@@ -14,7 +16,7 @@ public class TestReport extends TestCaseX2004 {
 		xq.arg("input").fromFile(new File("src/test/resources/com/tomecode/oracle/osb12c/x2004/sample.xml").getAbsoluteFile()).toSoapBody();
 		String out = xq.execute().asXmlString();
 		org.junit.Assert.assertEquals("<value><helloWorld xmlns:soap-env=\"http://schemas.xmlsoap.org/soap/envelope/\"/></value>", out);
-		
-		
+		report(xq).generate("hello");
+		Assert.assertEquals(true, new File("target/xqUnitTestReports/hello_report.html").getAbsoluteFile().exists());
 	}
 }
